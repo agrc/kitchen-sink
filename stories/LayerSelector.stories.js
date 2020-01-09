ea4e41a873497fe377a58f9319e70a58bd8ab6c8
steveoh <sgourley@utah.gov>
2020-01-09 12:18:10 -0700

    tests: allow esri modules to be used in stories

--- stories/LayerSelector.stories.js
+++ stories/LayerSelector.stories.js
@@ -1,37 +1,32 @@
 import React from 'react';
-import { LayerSelector } from '../packages/layer-selector/LayerSelector.js'
-import { loadModules } from 'esri-loader';
+import { LayerSelector, LayerSelectorContainer } from '../packages/layer-selector/LayerSelector.js'
+import getModules from './storyHelpers';
 
 export default {
   title: 'LayerSelector',
-  excludeStores: ['Basemap', 'LOD', 'TileInfo', 'WebTileLayer', 'FeatureLayer']
+  decorators: [getModules]
 };
 
-
-const Basemap = function () {
-  return {
-    baseLayers: {
-      getItemAt: () => { },
-      indexOf: () => { },
-      includes: () => { },
-      add: () => { },
-      remove: () => {},
-    }
-  }
-};
-const LOD = function () { };
-const TileInfo = function () { };
-const WebTileLayer = function () { };
-const FeatureLayer = function () { };
-
-export const basic = () => {
-
-  return (<LayerSelector
-    baseLayers={[{ name: 'Lite', selected: true, linked: false}, 'Terrain', 'Topo', 'Color IR']}
-    modules={{ LOD, TileInfo, WebTileLayer, Basemap, FeatureLayer }}
-    view={{ map: { basemap: { }, layers: { layerList: { getItemAt: () => { } } } }, when: () => { } }}
-    quadWord='test-fake-quad-word'
+export const basic = (modules) => (
+  <LayerSelector
+    baseLayers={['Lite', 'Terrain', 'Topo', 'Color IR']}
+    modules={modules}
+    view={{ map: {}, when: () => { } }}
+    quadWord='my-fake-quad-word'
   >
   </LayerSelector>
-  );
-};
+);
+
+export const inContainer = (modules) => (
+  <LayerSelector
+    baseLayers={['Lite', 'Terrain', 'Topo', 'Color IR']}
+    modules={modules}
+    view={{ map: {}, when: () => { } }}
+    quadWord='my-fake-quad-word'
+  >
+  </LayerSelector>
+);
+
+inContainer.story = {
+  decorators: [(story) => (<LayerSelectorContainer>{story()}</LayerSelectorContainer>)]
+}
