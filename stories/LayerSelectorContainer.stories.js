c0a763a1954e9af7dfd06c3d91253a5450f02582
steveoh <sgourley@utah.gov>
2020-01-08 20:00:26 -0700

    tests: add some layer selector stories

---
+++ stories/LayerSelectorContainer.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {LayerSelectorContainer} from '../packages/layer-selector/LayerSelector.js'
+
+
+export default {
+  title: 'LayerSelector.Container',
+};
+
+export const closed = () => (
+  <LayerSelectorContainer>
+    <div style={{width: '200px', height: '200px', border: '1px solid black'}}>peek-a-boo</div>
+  </LayerSelectorContainer>
+)
+
+export const open = () => (
+  <LayerSelectorContainer expanded={true}>
+    <div style={{ width: '200px', height: '200px', border: '1px solid black' }}>peek-a-boo</div>
+  </LayerSelectorContainer>
+)
