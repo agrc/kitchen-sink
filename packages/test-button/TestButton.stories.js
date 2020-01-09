e8ace3a606766ccd5f45013059e036626ae135b5
steveoh <sgourley@utah.gov>
2020-01-09 14:25:22 -0700

    refactor: move stories into packages

--- packages/test-button/TestButton.stories.js
+++ packages/test-button/TestButton.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
-import TestButton from '../packages/test-button';
+import TestButton from './index.js';
 
 export default {
   title: 'TestButton',
