c01f0f98df3cb1be6513f94e69845ec6eca94d56
Scott Davis <stdavis@utah.gov>
2020-01-10 12:35:10 -0700

    fix: fix basic linting errors

--- packages/layer-selector/LayerSelectorContainer.stories.js
+++ packages/layer-selector/LayerSelectorContainer.stories.js
@@ -3,17 +3,17 @@ import { LayerSelectorContainer } from './LayerSelector';
 
 
 export default {
-  title: 'LayerSelector.Container',
+  title: 'LayerSelector.Container'
 };
 
 export const closed = () => (
   <LayerSelectorContainer>
     <div style={{width: '200px', height: '200px', border: '1px solid black'}}>peek-a-boo</div>
   </LayerSelectorContainer>
-)
+);
 
 export const open = () => (
   <LayerSelectorContainer expanded={true}>
     <div style={{ width: '200px', height: '200px', border: '1px solid black' }}>peek-a-boo</div>
   </LayerSelectorContainer>
-)
+);
