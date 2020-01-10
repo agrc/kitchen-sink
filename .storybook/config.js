0da8992eb430560b4ca37ce837fc4c7d11d28ab1
steveoh <sgourley@utah.gov>
2020-01-09 19:00:40 -0700

    chore: remove dotenv since it's already there

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,4 @@
 import { configure } from '@storybook/react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../packages', true, /\.stories\.js$/), module);
