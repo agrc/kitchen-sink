da9291ec6eccf368ba9fc05a8b84e38b420cc05e
steveoh <sgourley@utah.gov>
2020-01-09 18:53:20 -0700

    chore: add dotenv

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../packages', true, /\.stories\.js$/), module);
