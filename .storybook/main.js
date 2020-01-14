268c101f54accfd41b7f251277f72a9951dea8b2
steveoh <sgourley@utah.gov>
2020-01-14 14:00:48 -0700

    tests: use new preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../packages/**/*.stories.js'],
   addons: [
     '@storybook/addon-actions',
-    '@storybook/addon-links'
+    '@storybook/addon-links',
+    '@storybook/preset-create-react-app'
   ]
 }
