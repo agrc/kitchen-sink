7878b8df6f8cd37e84cb6e7956c3b84544bc6818
Scott Davis <stdavis@utah.gov>
2020-01-29 08:57:22 -0700

    fix: add missing deps to effect

--- packages/layer-selector/LayerSelectorWithMap.stories.js
+++ packages/layer-selector/LayerSelectorWithMap.stories.js
@@ -38,7 +38,7 @@ const WithMap = ({ modules, baseLayers, overlays, scale, zoom, top, right, cente
         <LayerSelector {...layerSelectorOptions}></LayerSelector>
       </LayerSelectorContainer>,
       selectorNode);
-  }, [modules, mapDiv, center, zoom, scale]);
+  }, [modules, mapDiv, center, zoom, scale, top, right, overlays, baseLayers]);
 
   return (
     <div ref={mapDiv} style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}></div>
