6343fafaaaf65074275471124e64bec621036223
stdavis <stdavis@utah.gov>
2020-09-30 16:16:41 -0600

    fix(monorepo): remove un-used jest config

--- .jest/register-context.js
+++
@@ -1,6 +0,0 @@
-/*
-This file is required (no pun intended) to make `require.context(..)` work
-in .storybook/config.js.
-*/
-import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
-registerRequireContextHook();
