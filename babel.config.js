8eb0217a0671a5a442151973a78f9df15cbc6060
steveoh <sgourley@utah.gov>
2020-11-03 10:39:42 -0700

    chore(mono-repo): update storybook

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '@babel/preset-react'
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining'
   ],
   env: {
     prod: {
