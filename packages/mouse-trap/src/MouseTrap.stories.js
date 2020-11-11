9161d7245d475b31c397f705d2b7b35f08f177d9
steveoh <sgourley@utah.gov>
2020-11-10 17:03:36 -0700

    feat(mousetrap): mouse trap is now a react component
    BREAKING CHANGE

---
+++ packages/mouse-trap/src/MouseTrap.stories.js
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect, useState } from 'react';
+import MouseTrap from './MouseTrap';
+import EsriMap from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import { useMapReady } from '@agrc/helpers';
+import '@arcgis/core/assets/esri/themes/light/main.css';
+
+export default {
+  title: 'MouseTrap'
+};
+
+const WithMap = () => {
+  const mapDiv = useRef();
+  const [view, setView] = useState();
+  const ready = useMapReady(view);
+
+  useEffect(() => {
+    const esriMap = new EsriMap({
+      basemap: 'topo-vector'
+    });
+
+    const view = new MapView({
+      map: esriMap,
+      container: mapDiv.current,
+      zoom: 4,
+      center: [15, 65],
+      ui: {
+        components: ['zoom']
+      }
+    });
+    setView(view);
+  }, []);
+
+  return (<>
+    <div ref={mapDiv} style={{ width: '250px', height: '250px' }}>
+    </div>
+    { ready ?
+      <MouseTrap mapView={view} render={({ x, y }) => {
+        return <>
+          <span>x: {x}</span>
+          <span style={{paddingLeft: '1rem'}}>y: {y}</span>
+        </>;
+      }}></MouseTrap>
+      : null}
+  </>
+  );
+};
+
+export const defaultMouseTrap = () => <WithMap></WithMap>;
