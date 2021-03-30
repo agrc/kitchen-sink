0bd35a7ac9a06a580482f1e18a63da53cdeac820
stdavis <stdavis@utah.gov>
2021-03-30 14:57:26 -0600

    feat: add LocatorSuggestProvider to sherlock
    Includes a refactor to pull providers out into a separate file. Shouldn't break any existing apps.

---
+++ packages/sherlock/src/index.js
@@ -0,0 +1,5 @@
+import Sherlock from './Sherlock';
+import { WebApiProvider, MapServiceProvider, LocatorSuggestProvider } from './providers';
+
+
+export { Sherlock, WebApiProvider, MapServiceProvider, LocatorSuggestProvider };
