20eeeb84a3d0326b2489eecadd3e27a35ad551e9
steveoh <sgourley@utah.gov>
2022-06-23 18:35:54 -0600

    chore(monorepo): mark common libs as external and only export esm

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,13 @@ module.exports = defineConfig({
   build: {
     lib: {
       entry: path.resolve(directory, 'src/index.js'),
+      formats: ['esm'],
       name: `@ugrc/${packageName}`,
       fileName: (format) => `${packageName}.${format}.js`,
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled into your library
-      external: ['react'],
-      output: {
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: {
-          react: 'React',
-        },
-      },
+      external: ['react', 'prop-types', /@arcgis\/core\/.*/],
     },
   },
 });
