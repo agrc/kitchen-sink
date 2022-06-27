cf494161ae291ce31acf5457966d2b69818b6fc8
steveoh <sgourley@utah.gov>
2022-06-27 10:50:03 -0600

    refactor(monorepo)!: remove build and publish raw esm

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { basename, resolve } from 'path';
-
-const directory = process.cwd();
-const packageName = basename(directory);
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: resolve(directory, 'src/index.js'),
-      formats: ['esm'],
-      name: `@ugrc/${packageName}`,
-      fileName: (format) => `${packageName}.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled into your library
-      external: ['react', 'prop-types', /@arcgis\/core\/.*/],
-    },
-  },
   test: {
     environment: 'jsdom',
     globals: true,
