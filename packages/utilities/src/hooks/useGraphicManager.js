99e16a8b764700a7fa5e27dbf3aeee63a7bf4aa3
steveoh <sgourley@utah.gov>
2022-07-06 20:02:09 -0600

    fix(utilities): remove console.logs

--- packages/utilities/src/hooks/useGraphicManager.js
+++ packages/utilities/src/hooks/useGraphicManager.js
@@ -7,15 +7,12 @@ export default function useGraphicManager(mapView) {
   const removeGraphics = useCallback(
     (graphics) => {
       if (!graphics) {
-        console.log('no graphics to remove');
         return;
       }
 
       if (Array.isArray(graphics)) {
-        console.log('removeMany', graphics);
         graphics.forEach((x) => mapView.current.graphics.remove(x));
       } else {
-        console.log('remove', graphics);
         mapView.current.graphics.remove(graphics);
       }
     },
@@ -23,21 +20,18 @@ export default function useGraphicManager(mapView) {
   );
 
   useEffect(() => {
-    console.log('useGraphicManagerHook', graphic);
-
     removeGraphics(previousGraphic.current);
-
     previousGraphic.current = graphic;
 
     if (Array.isArray(graphic)) {
-      return mapView.current?.when(() => {
-        console.log('addMany', graphic);
+      mapView.current?.when(() => {
         mapView.current.graphics.addMany(graphic);
       });
+
+      return;
     }
 
     mapView.current?.when(() => {
-      console.log('add', graphic);
       mapView.current.graphics.add(graphic);
     });
   }, [graphic, removeGraphics, mapView]);
