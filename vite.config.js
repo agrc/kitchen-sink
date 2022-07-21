d1d4b580e19c30989445c198c7bc4087ee536f57
steveoh <sgourley@utah.gov>
2022-07-21 15:38:28 -0600

    fix: compile jsx to js for vite dev optimizations
    Co-authored-by: Scott Davis <stdavis@utah.gov>

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { basename, resolve } from 'path';
+import { readPackage } from 'read-pkg';
 
-export default defineConfig({
+const directory = process.cwd();
+const packageName = basename(directory);
+const packageJson = await readPackage({ cwd: directory });
+
+const rollupOptions = {
+  external: [
+    ...Object.keys(packageJson?.dependencies ?? {}),
+    ...Object.keys(packageJson?.peerDependencies ?? {}),
+    /@arcgis\/core\/.*/,
+  ],
+};
+
+if (packageName === 'utilities') {
+  rollupOptions.input = {
+    main: resolve(directory, 'src/index.js'),
+    hooks: resolve(directory, 'src/hooks/index.js'),
+  };
+}
+
+const config = defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(directory, 'src/index.js'),
+      formats: ['esm'],
+      name: `@ugrc/${packageName}`,
+      fileName: (format) => {
+        return `index.${format}.js`;
+      },
+    },
+    sourcemap: true,
+    rollupOptions,
+  },
   test: {
     environment: 'jsdom',
     globals: true,
   },
 });
+
+export default config;
