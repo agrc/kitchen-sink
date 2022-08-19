f90fdf67e5c41e3a2489f5552642ffde891b7e50
stdavis <stdavis@utah.gov>
2022-08-19 11:01:44 -0600

    feat(utilities): add useLocalStorage hook
    There are a variety of npm packages that solve this same problem but they were simple enough that I thought that it would be worth just writing our own exactly the way that we want it.

---
+++ packages/utilities/src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function (key, initialValue, parseWithJSON = false) {
+  const getValueFromLocalStorage = () => {
+    const value = localStorage.getItem(key);
+
+    return parseWithJSON ? JSON.parse(value) : value;
+  };
+
+  const [value, setValue] = useState(
+    () => getValueFromLocalStorage() ?? initialValue
+  );
+
+  const setNewValue = (newValue) => {
+    setValue(newValue);
+    localStorage.setItem(
+      key,
+      parseWithJSON ? JSON.stringify(newValue) : newValue
+    );
+  };
+
+  return [value, setNewValue];
+}
