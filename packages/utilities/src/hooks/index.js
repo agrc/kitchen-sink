f9bb166b9e45a10d00131c4096337376d4c1f2ad
stdavis <stdavis@utah.gov>
2022-08-23 09:13:26 -0600

    fix(utilities): sort imports

--- packages/utilities/src/hooks/index.js
+++ packages/utilities/src/hooks/index.js
@@ -1,9 +1,9 @@
 export { default as useGraphicManager } from './useGraphicManager';
 export { default as useInterval } from './useInterval';
+export { default as useLocalStorage } from './useLocalStorage';
 export { default as useMapReady } from './useMapReady';
 export { default as useOpenClosed } from './useOpenClosed';
 export { default as useViewLoading } from './useViewLoading';
 export { default as useViewPointZooming } from './useViewPointZooming';
 export { default as useViewUiPosition } from './useViewUiPosition';
 export { default as useWebMap } from './useWebMap';
-export { default as useLocalStorage } from './useLocalStorage';
