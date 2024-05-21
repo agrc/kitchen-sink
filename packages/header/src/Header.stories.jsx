76116e5fedbb13d23d378bfc6a41331242e596cf
stdavis <stdavis@utah.gov>
2024-05-21 12:59:59 -0600

    fix(header): hide external link icon for relative links

--- packages/header/src/Header.stories.jsx
+++ packages/header/src/Header.stories.jsx
@@ -4,6 +4,27 @@ export default {
   component: Header,
 };
 
+const links = [
+  {
+    title: 'Getting Started Guide',
+    actionUrl: {
+      url: 'https://gis.utah.gov',
+    },
+  },
+  {
+    title: 'Documentation',
+    actionUrl: {
+      url: '/docs',
+    },
+  },
+  {
+    title: 'Privacy Policy',
+    actionUrl: {
+      url: '/privacy',
+    },
+  },
+];
+
 export const DefaultHeader = {
-  render: () => <Header />,
+  render: () => <Header links={links} />,
 };
