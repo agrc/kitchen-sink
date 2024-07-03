fe8217e4b8a07c4ab30dd747aaffe18a76b679dd
steveoh <sgourley@utah.gov>
2024-07-03 15:46:48 -0600

    chore: storbook updates

---
+++ .storybook/main.js
@@ -0,0 +1,51 @@
+import path from 'node:path';
+
+const excludedProps = new Set([
+  'id',
+  'slot',
+  'onCopy',
+  'onCut',
+  'onPaste',
+  'onCompositionStart',
+  'onCompositionEnd',
+  'onCompositionUpdate',
+  'onSelect',
+  'onBeforeInput',
+  'onInput',
+]);
+
+export default {
+  stories: ['../packages/*/src/**/*.stories.{jsx,tsx}'],
+
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-themes'),
+  ],
+
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+
+  docs: 'tag',
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+      propFilter: (prop) =>
+        !prop.name.startsWith('aria-') && !excludedProps.has(prop.name),
+    },
+  },
+};
+
+function getAbsolutePath(value) {
+  return path.dirname(require.resolve(path.join(value, 'package.json')));
+}
