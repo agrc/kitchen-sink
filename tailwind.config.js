e3e4d3ce569f2eddd87501da6eac513441d460e6
steveoh <sgourley@utah.gov>
2024-07-03 15:47:21 -0600

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import ugrcTheme from './packages/tailwind/src/index';
+import rac from 'tailwindcss-react-aria-components';
+
+export default {
+  content: ['./packages/**/*.{js,jsx,ts,tsx}'],
+  darkMode: ['class'],
+  presets: [ugrcTheme],
+  plugins: [rac],
+};
