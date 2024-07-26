eb741e9b64aa05ebf1f00bc825f7280c82afd6b9
steveoh <sgourley@utah.gov>
2024-07-25 19:56:17 -0600

    feat(design): create checkbox and radio button components

---
+++ packages/utah-design-system/src/components/RadioGroup.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Form } from 'react-aria-components';
+import { Button } from './Button.tsx';
+import { Radio, RadioGroup } from './Radio.tsx';
+
+export default {
+  component: RadioGroup,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {},
+  args: {
+    label: 'Favorite sport',
+    isDisabled: false,
+    isRequired: false,
+    description: '',
+    children: (
+      <>
+        <Radio value="soccer">Soccer</Radio>
+        <Radio value="baseball">Baseball</Radio>
+        <Radio value="basketball">Basketball</Radio>
+      </>
+    ),
+  },
+};
+
+export const Default = {
+  args: {},
+};
+
+export const Validation = (args: any) => (
+  <form
+    onSubmit={(event) => {
+      event.preventDefault();
+    }}
+    className="flex flex-col items-start gap-2"
+  >
+    <RadioGroup {...args} />
+    <Button type="submit" variant="secondary">
+      Submit
+    </Button>
+  </form>
+);
+
+Validation.args = {
+  isRequired: true,
+};
