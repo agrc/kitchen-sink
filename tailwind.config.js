14aab22ec23ca81201a5ae97aa431d3ec3124450
steveoh <sgourley@utah.gov>
2024-07-25 19:57:20 -0600

    wip(layer-selector): create RAC layer selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 import ugrcTheme from './packages/tailwind-preset/src/index';
 import rac from 'tailwindcss-react-aria-components';
+import animate from 'tailwindcss-animate';
 
 export default {
   content: ['./packages/**/*.{js,jsx,ts,tsx}'],
   darkMode: ['class'],
   presets: [ugrcTheme],
-  plugins: [rac],
+  plugins: [rac, animate],
 };
