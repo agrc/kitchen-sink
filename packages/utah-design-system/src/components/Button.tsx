f2006cd4a1b92d1ac5103b2780673bcbe19220ca
steveoh <sgourley@utah.gov>
2024-07-25 19:56:33 -0600

    chore(design): move base styles around

--- packages/utah-design-system/src/components/Button.tsx
+++ packages/utah-design-system/src/components/Button.tsx
@@ -13,18 +13,18 @@ export interface ButtonProps extends RACButtonProps {
 
 let button = tv({
   extend: focusRing,
-  base: 'px-5 py-2 text-sm text-center transition rounded-full border border-black/10 dark:border-white/10 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.1)] dark:shadow-none cursor-default',
+  base: 'px-5 py-2 text-sm text-center transition border border-black/10 dark:border-white/10 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.1)] dark:shadow-none cursor-default',
   variants: {
     variant: {
       primary:
-        'bg-primary-900 hover:bg-primary-700 pressed:bg-primary-800 text-white',
+        'rounded-full bg-primary-900 hover:bg-primary-700 pressed:bg-primary-800 text-white',
       secondary:
-        'bg-secondary-800 hover:bg-secondary-600 pressed:bg-secondary-700 text-white',
+        'rounded-full bg-secondary-800 hover:bg-secondary-600 pressed:bg-secondary-700 text-white',
       accent:
-        'bg-accent-500 hover:bg-accent-300 pressed:bg-accent-400 text-zinc-900',
+        'rounded-full bg-accent-500 hover:bg-accent-300 pressed:bg-accent-400 text-zinc-900',
       destructive:
-        'bg-rose-700 hover:bg-rose-800 pressed:bg-rose-900 text-white',
-      icon: 'border-0 p-1 flex items-center justify-center text-zinc-900 hover:bg-black/5 pressed:bg-black/10 dark:text-zinc-400 dark:hover:bg-white/10 dark:pressed:bg-white/20 disabled:bg-transparent',
+        'rounded-full bg-rose-700 hover:bg-rose-800 pressed:bg-rose-900 text-white',
+      icon: 'border-0 p-1 flex items-center justify-center text-zinc-900 hover:bg-black/5 pressed:bg-black/10 dark:text-zinc-300 dark:hover:bg-white/10 dark:pressed:bg-white/20 disabled:bg-transparent',
     },
     isDisabled: {
       true: 'bg-zinc-100 dark:bg-zinc-500 text-zinc-500 dark:text-zinc-100 border-black/20 dark:border-white/20',
