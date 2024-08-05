94a57a2c9cfd8ffdd8805208629a7279946e2b9d
steveoh <sgourley@utah.gov>
2024-08-05 15:46:30 -0600

    style(social): improve focus colors

---
+++ packages/utah-design-system/src/components/SocialMedia.stories.jsx
@@ -0,0 +1,10 @@
+import { SocialMedia as Component } from './SocialMedia.jsx';
+
+export default {
+  component: Component,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Example = () => <Component />;
