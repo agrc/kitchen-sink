82624571b5d6dd7c46e9e708e770e081e6de3dee
steveoh <sgourley@utah.gov>
2024-09-05 15:09:50 -0600

    chore(avatar): remove anonymous prop

--- packages/utah-design-system/src/components/Avatar.stories.tsx
+++ packages/utah-design-system/src/components/Avatar.stories.tsx
@@ -9,17 +9,12 @@ export default {
 
 export const Example = (args: any) => <Avatar {...args} />;
 Example.args = {
-  anonymous: false,
   user: { email: 'ugrc-developers@utah.gov', displayName: 'UGRC Developers' },
 };
 
 export const Anonymous = (args: any) => <Avatar {...args} />;
-Anonymous.args = {
-  anonymous: true,
-};
 
 export const NoGravatar = (args: any) => <Avatar {...args} />;
 NoGravatar.args = {
-  anonymous: false,
   user: { email: 'test@test.com', displayName: 'Test User' },
 };
