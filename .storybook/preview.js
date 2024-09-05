e1f6803fecf993d1b8fbec004aeaf06531722bbc
steveoh <sgourley@utah.gov>
2024-09-05 11:13:55 -0600

    chore(monorepo): add firebase

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,28 @@
 import './tailwind.css';
 import { withThemeByClassName } from '@storybook/addon-themes';
 import { initialize, mswLoader } from 'msw-storybook-addon';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../packages/utah-design-system/firebase';
 
 initialize({ onUnhandledRequest: 'bypass' });
+const app = initializeApp(firebaseConfig);
+
+if (import.meta.env.DEV) {
+  const auth = getAuth(app);
+  if (typeof window === 'undefined' || !window['_firebase_auth_emulator']) {
+    try {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    } catch (error) {
+      console.log('auth emulator already connected', error);
+    }
+    if (typeof window !== 'undefined') {
+      window['_firebase_auth_emulator'] = true;
+    }
+  }
+}
 
 export const preview = {
   decorators: [
---
+++ packages/utah-design-system/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'apikey',
+  authDomain: 'localhost',
+  projectId: 'storybook',
+  storageBucket: 'storybook',
+  messagingSenderId: '1',
+  appId: '1:1:web:1',
+  measurementId: 'G-NA',
+};
