531e18f2def8593917b9cc55903d7c4a6251e120
steveoh <sgourley@utah.gov>
2024-09-10 15:20:30 -0600

    feat(menu): introduce a menu component

---
+++ packages/utah-design-system/src/components/Menu.tsx
@@ -0,0 +1,72 @@
+import { Check, ChevronRight } from 'lucide-react';
+import {
+  Menu as AriaMenu,
+  MenuItem as AriaMenuItem,
+  MenuProps as AriaMenuProps,
+  MenuItemProps,
+  Separator,
+  SeparatorProps,
+  composeRenderProps,
+} from 'react-aria-components';
+import {
+  DropdownSection,
+  DropdownSectionProps,
+  dropdownItemStyles,
+} from './ListBox';
+import { Popover, PopoverProps } from './Popover';
+
+interface MenuProps<T> extends AriaMenuProps<T> {
+  placement?: PopoverProps['placement'];
+}
+
+export function Menu<T extends object>(props: MenuProps<T>) {
+  return (
+    <Popover placement={props.placement} className="min-w-36">
+      <AriaMenu
+        {...props}
+        className="max-h-[inherit] overflow-auto p-1 outline outline-0 [clip-path:inset(0_0_0_0_round_.75rem)]"
+      />
+    </Popover>
+  );
+}
+
+export function MenuItem(props: MenuItemProps) {
+  return (
+    <AriaMenuItem {...props} className={dropdownItemStyles}>
+      {composeRenderProps(
+        props.children,
+        (children, { selectionMode, isSelected, hasSubmenu }) => (
+          <>
+            {selectionMode !== 'none' && (
+              <span className="flex w-4 items-center">
+                {isSelected && <Check aria-hidden className="h-auto w-4" />}
+              </span>
+            )}
+            <span className="flex flex-1 items-center gap-2 truncate font-normal group-selected:font-semibold">
+              {children}
+            </span>
+            {hasSubmenu && (
+              <ChevronRight
+                aria-hidden
+                className="absolute right-2 h-auto w-4"
+              />
+            )}
+          </>
+        ),
+      )}
+    </AriaMenuItem>
+  );
+}
+
+export function MenuSeparator(props: SeparatorProps) {
+  return (
+    <Separator
+      {...props}
+      className="mx-3 my-1 border-b border-gray-300 dark:border-zinc-700"
+    />
+  );
+}
+
+export function MenuSection<T extends object>(props: DropdownSectionProps<T>) {
+  return <DropdownSection {...props} />;
+}
