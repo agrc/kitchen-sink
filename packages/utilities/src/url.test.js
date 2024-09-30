6da3d3ad5d071d0b91f1508a3b45e341b15f4804
steveoh <sgourley@utah.gov>
2024-09-30 15:32:02 -0600

    test(url): make sure undefined or null values are not in the querystring

---
+++ packages/utilities/src/url.test.js
@@ -0,0 +1,15 @@
+import { toQueryString } from './url';
+import { describe, it, expect } from 'vitest';
+
+describe('toQueryString', () => {
+  it('should not include null and undefined values in the query string', () => {
+    const params = {
+      name: 'John',
+      age: null,
+      city: undefined,
+      country: 'USA',
+    };
+    const queryString = toQueryString(params);
+    expect(queryString).toBe('name=John&country=USA');
+  });
+});
