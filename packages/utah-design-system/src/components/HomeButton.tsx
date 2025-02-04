d9c94bd76ba1a98975aad57e423b758424120ddf
steveoh <sgourley@utah.gov>
2025-02-04 16:03:52 -0700

    feat(homebutton): allow custom actions when pressed

--- packages/utah-design-system/src/components/HomeButton.tsx
+++ packages/utah-design-system/src/components/HomeButton.tsx
@@ -7,10 +7,12 @@ export const HomeButton = ({
   view,
   position,
   initialExtent,
+  actions,
 }: {
   view: __esri.MapView;
   position?: __esri.UIAddComponent['position'];
   initialExtent?: __esri.Extent;
+  actions?: Function[];
 }) => {
   const goHome = useDefaultExtent(view, initialExtent);
   const uiPosition = useViewUiPosition(view, position ?? 'top-left');
@@ -24,7 +26,10 @@ export const HomeButton = ({
         variant="icon"
         className="size-full stroke-[4] p-0 transition-colors duration-150 ease-in-out will-change-transform group-hover:bg-[#f3f3f3] focus:min-h-0 focus:outline-offset-[-2px]"
         aria-label="Default extent"
-        onPress={() => goHome()}
+        onPress={() => {
+          goHome();
+          actions?.forEach((action) => action());
+        }}
       >
         <HomeIcon
           className="size-5 stroke-[1.5] text-[#6e6e6e] transition-colors duration-150 ease-in-out will-change-transform group-hover:text-[#151515]"
