6aaee94d534c9d6f954c566ac2af20ff4e18f47d
stdavis <stdavis@utah.gov>
2025-03-05 12:07:47 -0700

    feat(eslint-config): add eslint-config package

---
+++ packages/eslint-config/src/browser.js
@@ -0,0 +1,66 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11Y from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export const browser = tseslint.config(
+  {
+    ignores: [
+      '**/__mocks__',
+      '**/__snapshots__',
+      '**/node_modules',
+      '**/dist',
+      '**/package-lock.json',
+      '**/pnpm-lock.yaml',
+      '**/.vscode',
+      'public/*',
+    ],
+  },
+  importPlugin.flatConfigs.recommended,
+  jsxA11Y.flatConfigs.recommended,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  prettier,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // ref: https://typescript-eslint.io/troubleshooting/typed-linting/performance/#eslint-plugin-import
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+    },
+  },
+);
