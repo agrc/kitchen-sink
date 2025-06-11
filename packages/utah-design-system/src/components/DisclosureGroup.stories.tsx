bb10ac1ee824de4d804dd5ed283c8e3594661343
steveoh <sgourley@utah.gov>
2025-06-11 11:00:32 -0600

    chore(disclosure): add id's to add defaultExpanded

--- packages/utah-design-system/src/components/DisclosureGroup.stories.tsx
+++ packages/utah-design-system/src/components/DisclosureGroup.stories.tsx
@@ -13,21 +13,24 @@ const meta: Meta<typeof DisclosureGroup> = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  args: {
+    defaultExpandedKeys: ['files'],
+  },
 };
 
 export default meta;
 
 export const Example = (args: DisclosureGroupProps) => (
   <DisclosureGroup {...args}>
-    <Disclosure>
+    <Disclosure id="files">
       <DisclosureHeader>Files</DisclosureHeader>
       <DisclosurePanel>Files content</DisclosurePanel>
     </Disclosure>
-    <Disclosure isDisabled={true}>
+    <Disclosure id="images" isDisabled={true}>
       <DisclosureHeader>Images</DisclosureHeader>
       <DisclosurePanel>Images content</DisclosurePanel>
     </Disclosure>
-    <Disclosure>
+    <Disclosure id="documents">
       <DisclosureHeader>Documents</DisclosureHeader>
       <DisclosurePanel>Documents content</DisclosurePanel>
     </Disclosure>
