8a3b333df99bf3089132fc2abf720d09f0466cc4
stdavis <stdavis@utah.gov>
2025-10-16 10:50:48 -0600

    fix(uds): allow custom styles for Banner

--- packages/utah-design-system/src/components/Banner.tsx
+++ packages/utah-design-system/src/components/Banner.tsx
@@ -1,9 +1,20 @@
 import { TriangleAlertIcon } from 'lucide-react';
 import type { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export const Banner = ({ children }: { children: ReactNode }) => {
+type BannerProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export const Banner = ({ children, className }: BannerProps) => {
   return (
-    <div className="m-6 mx-auto flex min-h-[75px] max-w-lg flex-row gap-2 rounded border border-warning-500 bg-warning-50 dark:bg-warning-900/10">
+    <div
+      className={twMerge(
+        'm-6 mx-auto flex min-h-[75px] max-w-lg flex-row gap-2 rounded border border-warning-500 bg-warning-50 dark:bg-warning-900/10',
+        className,
+      )}
+    >
       <div className="inline-flex min-w-[75px] items-center justify-center rounded-l bg-warning-200 text-warning-500/70 dark:bg-warning-800/50">
         <TriangleAlertIcon className="h-full w-10" aria-hidden />
       </div>
